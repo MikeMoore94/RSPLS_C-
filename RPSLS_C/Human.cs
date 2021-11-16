bed432a728fae87881eb8fdb781605aebd0f8ed0
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-16 17:03:58 -0500

    added logic in the human class

--- RPSLS_C/Human.cs
+++ RPSLS_C/Human.cs
@@ -8,5 +8,19 @@ namespace RPSLS_C
 {
     internal class Human
     {
+        public Human(string name, int score, string gesture)
+        {
+            this.name = name;
+            this.score = score;
+            this.gesture = gesture;
+        }
+
+        public override string ChooseGesture()
+        {
+            Console.WriteLine("Attack!: 1 = Rock, 2 = Paper, 3 = Scissors, 4 = Lizard, and 5 = Spock");
+            int gesture = Convert.ToInt32(Console.ReadLine()) - 1;
+            return this.gestureList[gesture];
+
+        }
     }
 }
