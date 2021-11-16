68e9256e83c52514d23dacc5081396d24253f347
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-16 17:46:13 -0500

    added all methods to complete game, just have to add all the logic to them

--- RPSLS_C/Game.cs
+++ RPSLS_C/Game.cs
@@ -12,15 +12,48 @@ namespace RPSLS_C
         Player playerTwo;
         public Game()
         {
+            Player playerOne;
+            Player playerTwo;
+            public Game()
+        {
             this.playerOne = SetUpPlayerOne();
-            this.playerTwo = ChooseGameType();
+            this.playerTwo = Game_Type();
         }
 
-        public void RunGame()
+
+            public void RunGame()
         {
-            WelcomeMessage();
-            DisplayRules();
-            PlayGame();
+            Game_Rules();
+            Play_Game();
         }
+            public static Player SetUpPlayerOne()
+            {
+                Player playerOne;
+                Console.WriteLine(" Player One's name?: ");
+                string name = Console.ReadLine();
+                playerOne = new Human(name, 0, "none");
+                return playerOne;
+
+            }
+
+            public Player Game_Type()
+            {
+
+            }
+
+
+            public void Game_Rules()
+            {
+
+            }
+            public void Play_Game()
+            {
+
+            }
+
+            public void RoundWinner(string Player1Gesture, string Play22Gesture)
+            {
+                
+
     }
 }
